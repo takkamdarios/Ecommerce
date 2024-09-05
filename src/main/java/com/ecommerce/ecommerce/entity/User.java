455942d91f9d85ef4b9c7c84bb352d2b1243e740
takkamdarios <takkamdarios@gmail.com>
2024-09-05 11:18:45 +0200

    User Model

--- src/main/java/com/ecommerce/ecommerce/entity/User.java
+++ src/main/java/com/ecommerce/ecommerce/entity/User.java
@@ -1,4 +1,49 @@
 package com.ecommerce.ecommerce.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+    @Column(unique = true)
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Column(name = "phone_number")
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @OneToOne(fetch = Fetch.Type.LAZY, cascade = CascadeType.ALL)
+    private Address address;
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
 }
---
+++ src/main/java/com/ecommerce/ecommerce/enums/UserRole.java
@@ -0,0 +1,4 @@
+package com.ecommerce.ecommerce.enums;
+
+public enum UserRole {
+}
