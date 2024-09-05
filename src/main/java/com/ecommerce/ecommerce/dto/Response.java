a031d184f25d42e5fa3004d0851a2cf9d8098912
takkamdarios <takkamdarios@gmail.com>
2024-09-05 18:38:32 +0200

    Response entity

---
+++ src/main/java/com/ecommerce/ecommerce/dto/Response.java
@@ -0,0 +1,22 @@
+package com.ecommerce.ecommerce.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+
+    private int status;
+    private String message;
+    private final LocalDateTime timestamp = LocalDateTime.now();
+
+    private String token;
+    private String role;
+    private String expirationTime;
+}
---
+++ src/main/java/com/ecommerce/ecommerce/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.ecommerce.ecommerce.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private List<OrderItemDto> orderItems;
+
+
+}
