fdc869c6c6b0d701b65350496b882f49b85586bf
takkamdarios <takkamdarios@gmail.com>
2024-09-13 11:02:25 +0200

    User Repository

---
+++ src/main/java/com/ecommerce/ecommerce/exception/InvalideCredentialException.java
@@ -0,0 +1,4 @@
+package com.ecommerce.ecommerce.exception;
+
+public class InvalideCredentialException {
+}
---
+++ src/main/java/com/ecommerce/ecommerce/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerce.repository;
+
+import com.ecommerce.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
