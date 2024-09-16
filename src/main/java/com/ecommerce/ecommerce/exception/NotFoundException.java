7eeeccd6960bab59bebcb54ef41a4de98e1ddf6a
takkamdarios <takkamdarios@gmail.com>
2024-09-16 10:19:03 +0200

    exception

---
+++ src/main/java/com/ecommerce/ecommerce/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ecommerce.ecommerce.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
